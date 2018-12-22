ed2156e25fecbec5f286200055254136b6473d45
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2018-12-22 09:44:36 +0900

    Haskell with 10 minutes 2

--- Test.hs
+++ Test.hs
@@ -1,5 +1,9 @@
-main = do putStrLn "What is 2 + 2?"
+module Main where
+
+fractorial n = if n == 0 then 1 else n * fractorial (n - 1)
+
+main = do putStrLn "What is 5! ?"
           x <- readLn
-          if x == 4
+          if x == fractorial 5
             then putStrLn "You're right!"
             else putStrLn "You're wrong!"
